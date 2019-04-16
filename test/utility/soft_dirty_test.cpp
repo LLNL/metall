22cd0c3163292f8d46a2b2e1bdd5eea83fe9227b
Keita Iwabuchi <iwabuchi1@llnl.gov>
2019-04-15 17:03:01 -0700

    Minor code brush up in test/utility/soft_dirty_test.cpp

--- test/utility/soft_dirty_test.cpp
+++ test/utility/soft_dirty_test.cpp
@@ -7,6 +7,7 @@
 #include <metall/detail/utility/soft_dirty_page.hpp>
 #include <metall/detail/utility/mmap.hpp>
 #include <metall/detail/utility/file.hpp>
+#include <metall/detail/utility/memory.hpp>
 
 namespace {
 
@@ -23,26 +24,26 @@ void run_in_core_test(const std::size_t num_pages, char *const map) {
   ASSERT_GT(page_size, 0);
 
   const uint64_t page_no_offset = reinterpret_cast<uint64_t>(map) / page_size;
-  for (uint32_t i = 0; i < 2; ++i) {
+  for (uint64_t i = 0; i < 2; ++i) {
     ASSERT_TRUE(metall::detail::utility::reset_soft_dirty());
 
     {
       metall::detail::utility::pagemap_reader pr;
 
-      for (uint32_t p = 0; p < num_pages; ++p) {
+      for (uint64_t p = 0; p < num_pages; ++p) {
         ASSERT_NE(pr.at(page_no_offset + p), pr.error_value) << "Cannot get pagemap of " << p;
         ASSERT_FALSE(metall::detail::utility::check_soft_dirty(pr.at(page_no_offset + p))) << "Not clear at " << p;
       }
     }
 
-    for (uint32_t p = 0; p < num_pages; ++p) {
+    for (uint64_t p = 0; p < num_pages; ++p) {
       if (p % 2 == i % 2)
         map[page_size * p] = 0;
     }
 
     {
       metall::detail::utility::pagemap_reader pr;
-      for (uint32_t p = 0; p < num_pages; ++p) {
+      for (uint64_t p = 0; p < num_pages; ++p) {
         ASSERT_NE(pr.at(page_no_offset + p), pr.error_value) << "Cannot get pagemap of " << p;
         if (p % 2 == i % 2)
           ASSERT_TRUE(metall::detail::utility::check_soft_dirty(pr.at(page_no_offset + p))) << "Wrong value at " << p;
@@ -57,29 +58,34 @@ TEST(SoftDirtyTest, MapAnonymous) {
   const ssize_t page_size = metall::detail::utility::get_page_size();
   ASSERT_GT(page_size, 0);
 
-  char *map = static_cast<char *>(metall::detail::utility::map_anonymous_write_mode(nullptr, page_size * 4));
+  const std::size_t k_num_pages = 4;
+
+  char *map = static_cast<char *>(metall::detail::utility::map_anonymous_write_mode(nullptr, page_size * k_num_pages));
   ASSERT_TRUE(map);
 
-  run_in_core_test(4, map);
+  run_in_core_test(k_num_pages, map);
 
-  ASSERT_TRUE(metall::detail::utility::munmap(map, page_size * 4, false));
+  ASSERT_TRUE(metall::detail::utility::munmap(map, page_size * k_num_pages, false));
 }
 
 TEST(SoftDirtyTest, MapFileBacked) {
   const ssize_t page_size = metall::detail::utility::get_page_size();
   ASSERT_GT(page_size, 0);
 
-  metall::detail::utility::create_file("/tmp/soft_dirty_test_file");
-  metall::detail::utility::extend_file_size("/tmp/soft_dirty_test_file", page_size * 4);
+  const char * const k_file_path = "/tmp/soft_dirty_test_file";
+  const std::size_t k_num_pages = 4;
 
-  char *map = static_cast<char *>(metall::detail::utility::map_file_write_mode("/tmp/soft_dirty_test_file",
+  metall::detail::utility::create_file(k_file_path);
+  metall::detail::utility::extend_file_size(k_file_path, page_size * k_num_pages);
+
+  char *map = static_cast<char *>(metall::detail::utility::map_file_write_mode(k_file_path,
                                                                                nullptr,
-                                                                               page_size * 4,
+                                                                               page_size * k_num_pages,
                                                                                0).second);
   ASSERT_TRUE(map);
 
-  run_in_core_test(4, map);
+  run_in_core_test(k_num_pages, map);
 
-  ASSERT_TRUE(metall::detail::utility::munmap(map, page_size * 4, false));
+  ASSERT_TRUE(metall::detail::utility::munmap(map, page_size * k_num_pages, false));
 }
 }
