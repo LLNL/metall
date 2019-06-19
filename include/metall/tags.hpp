b5b9e5e693fe96aa5208fc029d57556bb777abe9
Keita Iwabuchi <kiwabuchi@llnl.gov>
2019-06-19 10:41:41 -0700

    Removed warnings about unused variables

--- include/metall/tags.hpp
+++ include/metall/tags.hpp
@@ -8,41 +8,40 @@
 
 #include <metall/detail/utility/char_ptr_holder.hpp>
 
-namespace metall
-{
+namespace metall {
 /// \brief Tag to create the segment always.
 ///
 /// The existing segment with the same name is over written.
 struct create_only_t {};
-static const create_only_t create_only{};
+[[maybe_unused]] static const create_only_t create_only{};
 
 /// \brief Tag to open an already created segment.
 struct open_only_t {};
-static const open_only_t open_only{};
+[[maybe_unused]] static const open_only_t open_only{};
 
 /// \brief Tag to open a segment if it exists.
 ///
 /// If it does not exist, creates a new one
 struct open_or_create_t {};
-static const open_or_create_t open_or_create{};
+[[maybe_unused]] static const open_or_create_t open_or_create{};
 
 /// \brief Not implemented
 struct open_read_only_t {};
-static const open_read_only_t open_read_only{};
+[[maybe_unused]] static const open_read_only_t open_read_only{};
 
 /// \brief Not implemented
 struct open_read_private_t {};
-static const open_read_private_t open_read_private{};
+[[maybe_unused]] static const open_read_private_t open_read_private{};
 
 /// \brief Not implemented
 struct open_copy_on_write_t {};
-static const open_copy_on_write_t open_copy_on_write{};
+[[maybe_unused]] static const open_copy_on_write_t open_copy_on_write{};
 
 /// \brief Tag to construct anonymous instances.
-static const detail::utility::anonymous_instance_t * anonymous_instance = nullptr;
+[[maybe_unused]] static const detail::utility::anonymous_instance_t *anonymous_instance = nullptr;
 
 /// \brief Tag to construct a unique instance of a type
-static const detail::utility::unique_instance_t * unique_instance = nullptr;
+[[maybe_unused]] static const detail::utility::unique_instance_t *unique_instance = nullptr;
 }
 
 #endif //METALL_TAGS_HPP
