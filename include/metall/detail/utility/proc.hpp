62d18e92ef9025d999ca59ebab3484c00e3ac832
Keita Iwabuchi <kiwabuchi@llnl.gov>
2020-01-27 11:02:18 -0800

    Use sched_getcpu instead of getcpu

--- include/metall/detail/utility/proc.hpp
+++ include/metall/detail/utility/proc.hpp
@@ -6,9 +6,7 @@
 #ifndef METALL_DETAIL_UTILITY_PROC_HPP
 #define METALL_DETAIL_UTILITY_PROC_HPP
 
-#if defined(__linux__)
-#include <linux/getcpu.h>
-#endif
+#include <sched.h>
 
 namespace metall {
 namespace detail {
@@ -17,32 +15,15 @@ namespace utility {
 /// \brief Returns the number of the CPU core on which the calling thread is currently executing
 /// \return Returns a nonnegative CPU core number
 inline int get_cpu_core_no() {
-#if defined(__linux__)
-  int cpu;
-  if (::getcpu(&cpu, nullptr, nullptr) == -1) {
+#if defined(_GNU_SOURCE)
+  const int cpu = sched_getcpu();
+  if (cpu == -1) {
     return 0;
   }
   return cpu;
 #else
 #ifdef METALL_VERBOSE_SYSTEM_SUPPORT_WARNING
 #warning "CPU core number is always 0"
-#endif
-  return 0;
-#endif
-}
-
-/// \brief Returns the number of the NUMA node on which the calling thread is currently executing
-/// \return Returns a nonnegative NUMA node number
-inline int get_numa_node_num() {
-#if defined(__linux__)
-  int numa_node;
-  if (::getcpu(&nullptr, &numa_node, nullptr) == -1) {
-    return 0;
-  }
-  return numa_node;
-#else
-#ifdef METALL_VERBOSE_SYSTEM_SUPPORT_WARNING
-#warning "NUMA node number is always 0"
 #endif
   return 0;
 #endif
