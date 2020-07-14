14d0d841378d88b76d5c660460cb3c7a494ca0a6
Keita Iwabuchi <kiwabuchi@llnl.gov>
2020-07-13 19:12:12 -0700

    Change to open Metall as the read only mode in bench/adjacent_list/test

--- bench/adjacency_list/test/open_metall.cpp
+++ bench/adjacency_list/test/open_metall.cpp
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
   }
 
   {
-    metall::manager manager(metall::open_only, option.datastore_path_list[0].c_str());
+    metall::manager manager(metall::open_read_only, option.datastore_path_list[0].c_str());
     auto ret = manager.find<adjacency_list_type>(option.adj_list_key_name.c_str());
     if (ret.second != 1) {
       std::cerr << "Its length is not correct" << std::endl;
