e203c09a2fb4153ae4dd05a9e225f36731b16fdf
Keita Iwabuchi <kiwabuchi@llnl.gov>
2020-09-02 19:01:49 -0700

    Bugfix to build on macOS >= 10.15 with clang

--- cmake/include_cxx_filesystem_library.cmake
+++ cmake/include_cxx_filesystem_library.cmake
@@ -5,30 +5,29 @@ function(include_cxx_filesystem_library)
     # Check if C++17 <filesystem> header files are available
     # If not, uses our own implementation.
     include(CheckIncludeFileCXX)
-    CHECK_INCLUDE_FILE_CXX(filesystem FILESYSTEM_INCLUDE_FILE)
-    if (NOT FILESYSTEM_INCLUDE_FILE)
+    CHECK_INCLUDE_FILE_CXX(filesystem FOUND_FILESYSTEM_HEADER)
+    if (NOT FOUND_FILESYSTEM_HEADER)
         message(STATUS "Cannot find the C++17 <filesystem> library. Use own implementation.")
         add_definitions(-DMETALL_NOT_USE_CXX17_FILESYSTEM_LIB)
         return()
     endif ()
 
     # Find the correct option to link the C++17 <filesystem> library
-    # GCC: stdc++fs
-    # Clang + macOS >= 10.15: c++fs
-    # Clang + macOS < 10.15: uses our own implementation
-    # Clang + the other OS: c++fs
+    # GCC
+    #   Any platform: stdc++fs
+    # LLVM
+    #   Assumes LLVM >= 9.0 and libc++ >= 7.0.
+    #   Clang + macOS >= 10.15: nothing special is required to use <filesystem>
+    #   Clang + macOS < 10.15: uses our own implementation
+    #   Clang + the other OS: nothing special is required to use <filesystem>
     if (("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")) # GCC
         link_libraries(stdc++fs)
     elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")  # Clang or AppleClang
-        if (NOT APPLE) # Not macOS
-            link_libraries(c++fs)
-        else()
+        if (APPLE) # macOS
             include(get_macos_version)
-            get_macos_version() # Get MacOS version
+            get_macos_version() # Get macOS version
             message(VERBOSE "macOS version ${MACOS_VERSION}")
-            if (MACOS_VERSION VERSION_GREATER_EQUAL 10.15) # macOS >= 10.15
-                link_libraries(c++fs)
-            else () # macOS < 10.15
+            if (MACOS_VERSION VERSION_LESS 10.15) # macOS < 10.15
                 message(STATUS "macOS >= 10.15 is required to use the C++17 <filesystem> library. Use own implementation.")
                 add_definitions(-DMETALL_NOT_USE_CXX17_FILESYSTEM_LIB)
             endif ()
