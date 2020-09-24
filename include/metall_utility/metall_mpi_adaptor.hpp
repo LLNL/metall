cc5e1a1fbe1bb55d5c3018ef156fb1fee5642d71
Keita Iwabuchi <kiwabuchi@llnl.gov>
2020-09-23 21:44:02 -0700

    Update MPI adaptor

--- include/metall_utility/metall_mpi_adaptor.hpp
+++ include/metall_utility/metall_mpi_adaptor.hpp
@@ -23,6 +23,8 @@ class metall_mpi_adaptor {
   /// \brief Metall manager type
   using manager_type = metall::manager;
 
+  static constexpr const char *k_mpi_size_file_name = "metall_mpi_adaptor_mpi_size";
+
   // -------------------------------------------------------------------------------- //
   // Constructor & assign operator
   // -------------------------------------------------------------------------------- //
@@ -30,6 +32,7 @@ class metall_mpi_adaptor {
       : m_mpi_comm(comm),
         m_local_dir_path(make_local_dir_path(data_store_dir, comm)),
         m_local_metall_manager(nullptr) {
+    check_mpi_size(data_store_dir, comm);
     m_local_metall_manager = std::make_unique<manager_type>(metall::open_only, m_local_dir_path.c_str());
   }
 
@@ -37,6 +40,7 @@ class metall_mpi_adaptor {
       : m_mpi_comm(comm),
         m_local_dir_path(make_local_dir_path(data_store_dir, comm)),
         m_local_metall_manager(nullptr) {
+    check_mpi_size(data_store_dir, comm);
     m_local_metall_manager = std::make_unique<manager_type>(metall::open_read_only, m_local_dir_path.c_str());
   }
 
@@ -93,7 +97,25 @@ class metall_mpi_adaptor {
   }
 
   static bool remove(const char *dir_path, const MPI_Comm &comm = MPI_COMM_WORLD) {
-    return manager_type::remove(make_local_dir_path(dir_path, comm).c_str());
+    const int rank = mpi_comm_rank(comm);
+    const int size = mpi_comm_size(comm);
+
+    bool ret = true;
+    for (int i = 0; i < size; ++i) {
+      if (i == rank) {
+        if (metall::detail::utility::file_exist(dir_path) && !metall::detail::utility::remove_file(dir_path)) {
+          std::cerr << "Failed to remove directory: " << dir_path << std::endl;
+          ret = false;
+        }
+      }
+      mpi_barrier(comm);
+    }
+    return global_or(ret, comm);
+  }
+
+  /// \brief Returns the correct MPI size to open the datastore
+  static int size(const char *dir_path, const MPI_Comm &comm = MPI_COMM_WORLD) {
+    return read_mpi_size(dir_path, comm);
   }
 
  private:
@@ -104,17 +126,66 @@ class metall_mpi_adaptor {
   // -------------------------------------------------------------------------------- //
   static void setup_inter_level_dir(const std::string &base_dir_path, const MPI_Comm &comm) {
     const int rank = mpi_comm_rank(comm);
+    const int size = mpi_comm_size(comm);
 
-    for (int i = 0; i < mpi_comm_size(comm); ++i) {
+    for (int i = 0; i < size; ++i) {
       if (i == rank) {
         const std::string path = make_inter_level_dir_path(base_dir_path);
         if (!metall::detail::utility::file_exist(path) && !metall::detail::utility::create_directory(path)) {
           std::cerr << "Failed to create directory: " << path << std::endl;
           ::MPI_Abort(comm, -1);
         }
+        // To simplify the code, all processes do the same thing.
+        store_mpi_size(base_dir_path, comm);
       }
       mpi_barrier(comm);
     }
+  }
+
+  static void store_mpi_size(const std::string &base_dir_path, const MPI_Comm &comm) {
+    const int size = mpi_comm_size(comm);
+    const std::string path = make_inter_level_dir_path(base_dir_path) + "/" + k_mpi_size_file_name;
+
+    std::ofstream ofs(path);
+    if (!ofs) {
+      std::cerr << "Failed to create a file: " << path << std::endl;
+      ::MPI_Abort(comm, -1);
+    }
+    ofs << size;
+    if (!ofs) {
+      std::cerr << "Failed to write data to a file: " << path << std::endl;
+      ::MPI_Abort(comm, -1);
+    }
+    ofs.close();
+  }
+
+  static int read_mpi_size(const std::string &base_dir_path, const MPI_Comm &comm) {
+    const std::string path = make_inter_level_dir_path(base_dir_path) + "/" + k_mpi_size_file_name;
+    std::ifstream ifs(path);
+    if (!ifs) {
+      std::cerr << "Failed to open a file: " << path << std::endl;
+      ::MPI_Abort(comm, -1);
+    }
+    int read_size = -1;
+    if (!(ifs >> read_size)) {
+      std::cerr << "Failed to read data from: " << path << std::endl;
+      ::MPI_Abort(comm, -1);
+    }
+
+    return read_size;
+  }
+
+  static void check_mpi_size(const std::string &base_dir_path, const MPI_Comm &comm) {
+    const int rank = mpi_comm_rank(comm);
+    const int size = mpi_comm_size(comm);
+
+    if (rank == 0) {
+      if (read_mpi_size(base_dir_path, comm) != size) {
+        std::cerr << "Invalid number of MPI processes" << std::endl;
+        ::MPI_Abort(comm, -1);
+      }
+    }
+    mpi_barrier(comm);
   }
 
   static std::string make_inter_level_dir_path(const std::string &base_dir_path) {
