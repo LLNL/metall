ed52f999eba4e1e51f116362166949599b85f291
Keita Iwabuchi <kiwabuchi@llnl.gov>
2020-10-05 21:44:59 -0700

    Update examples and test programs for clang

--- test/kernel/multimanager_test.cpp
+++ test/kernel/multimanager_test.cpp
@@ -28,12 +28,6 @@ using metall_allocator = typename manager_type::allocator_type<T>;
 TEST(MultiManagerTest, SingleThread) {
   using element_type = uint64_t;
   using vector_type = boost::interprocess::vector<element_type, metall_allocator<element_type>>;
-  using map_type = boost::unordered_map<element_type, // Key
-                                        vector_type, // Value
-                                        std::hash<element_type>, // Hash function
-                                        std::equal_to<>, // Equal function
-                                        boost::container::scoped_allocator_adaptor<metall_allocator<std::pair<const element_type,
-                                                                                                              vector_type>>>>;
 
   const auto dir_path1(test_utility::make_test_dir_path(::testing::UnitTest::GetInstance()->current_test_info()->name()
                                                             + std::to_string(1)));
@@ -44,56 +38,56 @@ TEST(MultiManagerTest, SingleThread) {
     manager_type manager1(metall::create_only, dir_path1.c_str(), k_chunk_size * 8);
     manager_type manager2(metall::create_only, dir_path2.c_str(), k_chunk_size * 8);
 
-    map_type *map1 = manager1.construct<map_type>("map")(manager1.get_allocator<>());
-    map_type *map2 = manager2.construct<map_type>("map")(manager2.get_allocator<>());
+    vector_type *vector1 = manager1.construct<vector_type>("vector")(manager1.get_allocator<>());
+    vector_type *vector2 = manager2.construct<vector_type>("vector")(manager2.get_allocator<>());
 
-    (*map1)[0].emplace_back(1);
-    (*map1)[0].emplace_back(2);
+    vector1->emplace_back(1);
+    vector1->emplace_back(2);
 
-    (*map2)[0].emplace_back(3);
-    (*map2)[0].emplace_back(4);
+    vector2->emplace_back(3);
+    vector2->emplace_back(4);
   }
 
   {
     manager_type manager1(metall::open_only, dir_path1.c_str());
     manager_type manager2(metall::open_only, dir_path2.c_str());
 
-    map_type *map1;
+    vector_type *vector1;
     std::size_t n1;
-    std::tie(map1, n1) = manager1.find<map_type>("map");
+    std::tie(vector1, n1) = manager1.find<vector_type>("vector");
 
-    map_type *map2;
+    vector_type *vector2;
     std::size_t n2;
-    std::tie(map2, n2) = manager2.find<map_type>("map");
+    std::tie(vector2, n2) = manager2.find<vector_type>("vector");
 
-    ASSERT_EQ((*map1)[0][0], 1);
-    ASSERT_EQ((*map1)[0][1], 2);
-    (*map1)[1].emplace_back(5);
+    ASSERT_EQ(vector1->at(0), 1);
+    ASSERT_EQ(vector1->at(1), 2);
+    vector1->emplace_back(5);
 
-    ASSERT_EQ((*map2)[0][0], 3);
-    ASSERT_EQ((*map2)[0][1], 4);
-    (*map2)[1].emplace_back(6);
+    ASSERT_EQ(vector2->at(0), 3);
+    ASSERT_EQ(vector2->at(1), 4);
+    vector2->emplace_back(6);
   }
 
   {
     manager_type manager1(metall::open_only, dir_path1.c_str());
     manager_type manager2(metall::open_only, dir_path2.c_str());
 
-    map_type *map1;
+    vector_type *vector1;
     std::size_t n1;
-    std::tie(map1, n1) = manager1.find<map_type>("map");
+    std::tie(vector1, n1) = manager1.find<vector_type>("vector");
 
-    map_type *map2;
+    vector_type *vector2;
     std::size_t n2;
-    std::tie(map2, n2) = manager2.find<map_type>("map");
+    std::tie(vector2, n2) = manager2.find<vector_type>("vector");
 
-    ASSERT_EQ((*map1)[0][0], 1);
-    ASSERT_EQ((*map1)[0][1], 2);
-    ASSERT_EQ((*map1)[1][0], 5);
+    ASSERT_EQ(vector1->at(0), 1);
+    ASSERT_EQ(vector1->at(1), 2);
+    ASSERT_EQ(vector1->at(2), 5);
 
-    ASSERT_EQ((*map2)[0][0], 3);
-    ASSERT_EQ((*map2)[0][1], 4);
-    ASSERT_EQ((*map2)[1][0], 6);
+    ASSERT_EQ(vector2->at(0), 3);
+    ASSERT_EQ(vector2->at(1), 4);
+    ASSERT_EQ(vector2->at(2), 6);
   }
 }
 
@@ -111,12 +105,6 @@ int get_num_threads() {
 TEST(MultiManagerTest, MultiThread) {
   using element_type = uint64_t;
   using vector_type = boost::interprocess::vector<element_type, metall_allocator<element_type>>;
-  using map_type = boost::unordered_map<element_type, // Key
-                                        vector_type, // Value
-                                        std::hash<element_type>, // Hash function
-                                        std::equal_to<>, // Equal function
-                                        boost::container::scoped_allocator_adaptor<metall_allocator<std::pair<const element_type,
-                                                                                                              vector_type>>>>;
 
   OMP_DIRECTIVE(parallel)
   {
@@ -124,10 +112,10 @@ TEST(MultiManagerTest, MultiThread) {
                                                              + std::to_string(omp::get_thread_num())));
 
     manager_type manager(metall::create_only, dir_path.c_str(), k_chunk_size * 16);
-    map_type *map = manager.construct<map_type>("map")(manager.get_allocator<>());
+    vector_type *vector = manager.construct<vector_type>("vector")(manager.get_allocator<>());
 
     for (int i = 0; i < 64; ++i) {
-      (*map)[i % 8].emplace_back(i * omp::get_thread_num());
+      vector->emplace_back(i * omp::get_thread_num());
     }
   }
 
@@ -136,12 +124,12 @@ TEST(MultiManagerTest, MultiThread) {
                                                              + std::to_string(t)));
     manager_type manager(metall::open_only, dir_path.c_str());
 
-    map_type *map;
+    vector_type *vector;
     std::size_t n;
-    std::tie(map, n) = manager.find<map_type>("map");
+    std::tie(vector, n) = manager.find<vector_type>("vector");
 
     for (int i = 0; i < 64; ++i) {
-      ASSERT_EQ((*map)[i % 8][i / 8], i * t);
+      ASSERT_EQ(vector->at(i), i * t);
     }
   }
 }
