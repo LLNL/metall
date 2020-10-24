e1660a3e0493000658d20d095f699aefa4be5898
Keita Iwabuchi <kiwabuchi@llnl.gov>
2020-10-23 21:51:02 -0700

    [skip ci] Code brush-up in bench script

--- bench/adjacency_list/run_bench.sh
+++ bench/adjacency_list/run_bench.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
+# ------------------------- #
+# Usage
+# ------------------------- #
 # cd metall/build/bench/adjacency_list
-# sh ../../../bench/adjacency_list/run_bench.sh
+# sh ../../../bench/adjacency_list/run_bench.sh [options]
 
-# ----- Options----- #
-VERTEX_SCALE=17
+# ------------------------- #
+# Options
+# ------------------------- #
+VERTEX_SCALE=17        # RMAT graph SCALE
 FILE_SIZE=$((2 ** 30)) # File size for Boost.Interprocess
 LOG_FILE_PREFIX="out_adj_bench"
 NUM_THREADS=""
-SCHEDULE=""
+OMP_SCHEDULE=""
 case "$OSTYPE" in
 darwin*) DATASTORE_DIR_ROOT="/tmp" ;;
 linux*) DATASTORE_DIR_ROOT="/dev/shm" ;;
@@ -24,7 +29,7 @@ while getopts "v:f:l:t:s:d:n:cp:E:" OPT; do
   f) FILE_SIZE=$OPTARG ;;
   l) LOG_FILE_PREFIX=$OPTARG ;;
   t) NUM_THREADS="env OMP_NUM_THREADS=${OPTARG}" ;;
-  s) SCHEDULE="env OMP_SCHEDULE=${OPTARG}" ;;
+  s) OMP_SCHEDULE="env OMP_SCHEDULE=${OPTARG}" ;;
   d) DATASTORE_DIR_ROOT=$OPTARG ;;
   n) CHUNK_SIZE=$OPTARG ;;
   c) NO_CLEANING_FILES_AT_END=true ;;
@@ -35,7 +40,9 @@ while getopts "v:f:l:t:s:d:n:cp:E:" OPT; do
   esac
 done
 
-# ----- Configurations ----- #
+# ------------------------- #
+# Configurations
+# ------------------------- #
 A=0.57
 B=0.19
 C=0.19
@@ -57,22 +64,17 @@ DATASTORE_NAME="metall_adjlist_bench"
 #    esac ;;
 #esac
 
+# ------------------------- #
+# Utility functions
+# ------------------------- #
+# Makes a directory if it does not exit
 make_dir() {
   if [ ! -d "$1" ]; then
     mkdir -p $1
   fi
 }
 
-try_to_get_compiler_ver() {
-  local exec_file_name=$1
-
-  echo "" | tee -a ${LOG_FILE}
-  echo "----------------------------------------" | tee -a ${LOG_FILE}
-  echo "Compiler information in" ${exec_file_name} | tee -a ${LOG_FILE}
-  echo "----------------------------------------" | tee -a ${LOG_FILE}
-  strings ${exec_file_name} | grep "GCC" | tee -a ${LOG_FILE}
-}
-
+# Executes a command, showing some information
 execute() {
   echo "Command: " "$@" | tee -a ${LOG_FILE}
   echo ">>>>>" | tee -a ${LOG_FILE}
@@ -80,98 +82,85 @@ execute() {
   echo "<<<<<" | tee -a ${LOG_FILE}
 }
 
+# Executes a command, showing some information
 execute_simple() {
   echo "$@" | tee -a ${LOG_FILE}
   "$@" | tee -a ${LOG_FILE}
+  echo "" | tee -a ${LOG_FILE}
 }
 
+# Logs a string with a heading style
 log_header() {
   echo "" | tee -a ${LOG_FILE}
   echo "----------------------------------------" | tee -a ${LOG_FILE}
   echo "$@" | tee -a ${LOG_FILE}
   echo "----------------------------------------" | tee -a ${LOG_FILE}
+  echo "" | tee -a ${LOG_FILE}
 }
 
-get_system_info() {
-  echo "" | tee -a ${LOG_FILE}
-  echo "----------------------------------------" | tee -a ${LOG_FILE}
-  echo "System information" | tee -a ${LOG_FILE}
-  echo "----------------------------------------" | tee -a ${LOG_FILE}
+# Tries to get used compiler version from a given executable
+try_to_get_compiler_info() {
+  local exec_file_name=$1
+  log_header "Compiler information in ${exec_file_name}"
+  execute_simple sh -c "strings ${exec_file_name} | grep GCC"
+}
 
-  echo "free -g" | tee -a ${LOG_FILE}
+# Logs some system information
+log_system_info() {
   if [[ $OSTYPE == "linux"* ]]; then
-    free -g | tee -a ${LOG_FILE}
+    log_header "DRAM Usage"
+    execute_simple free -g
+
+    log_header "VM Configuration"
+    execute_simple cat /proc/sys/vm/dirty_expire_centisecs
+    execute_simple cat /proc/sys/vm/dirty_ratio
+    execute_simple cat /proc/sys/vm/dirty_background_ratio
+    execute_simple cat /proc/sys/vm/dirty_writeback_centisecs
   fi
 
-  echo "" | tee -a ${LOG_FILE}
-  echo "/proc/sys/vm/dirty_expire_centisecs" | tee -a ${LOG_FILE}
-  cat /proc/sys/vm/dirty_expire_centisecs | tee -a ${LOG_FILE}
-  echo "/proc/sys/vm/dirty_ratio" | tee -a ${LOG_FILE}
-  cat /proc/sys/vm/dirty_ratio | tee -a ${LOG_FILE}
-  echo "/proc/sys/vm/dirty_background_ratio" | tee -a ${LOG_FILE}
-  cat /proc/sys/vm/dirty_background_ratio | tee -a ${LOG_FILE}
-  echo "/proc/sys/vm/dirty_writeback_centisecs" | tee -a ${LOG_FILE}
-  cat /proc/sys/vm/dirty_writeback_centisecs | tee -a ${LOG_FILE}
-
-  echo "" | tee -a ${LOG_FILE}
-  echo "" | tee -a ${LOG_FILE}
-  df -lh | tee -a ${LOG_FILE}
-  mount | tee -a ${LOG_FILE}
-
-  echo "" | tee -a ${LOG_FILE}
+  log_header "Storage Information"
+  execute_simple df -lh
+  execute_simple mount
 }
 
-run() {
-  # ------------------------- #
-  # Configure
-  # ------------------------- #
+# ------------------------- #
+# Main
+# ------------------------- #
+main() {
+  #  ----- Setup ----- #
+  log_header "Benchmark Setup"
   LOG_FILE=${LOG_FILE_PREFIX}"_"${EXEC_NAME}".log"
   echo "" >${LOG_FILE} # Reset the log file
 
   local datastore_dir=${DATASTORE_DIR_ROOT}/${EXEC_NAME}
   make_dir ${datastore_dir}
-  rm -f "${datastore_dir}/*" # Erase old data if they exist
+  execute_simple rm -f "${datastore_dir}/*" # Erase old data if they exist
 
-  # ------------------------- #
-  # Show system inf
-  # ------------------------- #
-  get_system_info
+  # ----- Show system info ----- #
+  log_system_info
   #    ${DROP_PAGECACHE_COMMAND}
 
-  # ------------------------- #
-  # Benchmark main
-  # ------------------------- #
+  # ----- Benchmark main ----- #
   local exec_file_name="../adjacency_list/run_adj_list_bench_${EXEC_NAME}"
-  echo "Run the benchmark using" ${exec_file_name} | tee -a ${LOG_FILE}
 
-  try_to_get_compiler_ver ${exec_file_name}
+  try_to_get_compiler_info ${exec_file_name}
 
-  echo "" | tee -a ${LOG_FILE}
-  echo "----------------------------------------" | tee -a ${LOG_FILE}
-  echo "Start benchmark" | tee -a ${LOG_FILE}
-  echo "----------------------------------------" | tee -a ${LOG_FILE}
+  log_header "Start benchmark"
   local datastore_path=${datastore_dir}/${DATASTORE_NAME}
   local num_edges=$((2 ** $((${VERTEX_SCALE} + 4))))
-  execute ${NUM_THREADS} ${SCHEDULE} ${UMAP_PAGESIZE} ${exec_file_name} -o ${datastore_path} -f ${FILE_SIZE} -s ${RND_SEED} -v ${VERTEX_SCALE} -e ${num_edges} -a ${A} -b ${B} -c ${C} -r 1 -u 1 -n ${CHUNK_SIZE} -V
+  execute ${NUM_THREADS} ${OMP_SCHEDULE} ${UMAP_PAGESIZE} ${exec_file_name} -o ${datastore_path} -f ${FILE_SIZE} -s ${RND_SEED} -v ${VERTEX_SCALE} -e ${num_edges} -a ${A} -b ${B} -c ${C} -r 1 -u 1 -n ${CHUNK_SIZE} -V
 
-  echo "" | tee -a ${LOG_FILE}
-  echo "----------------------------------------" | tee -a ${LOG_FILE}
-  echo "Datastore information" | tee -a ${LOG_FILE}
-  echo "----------------------------------------" | tee -a ${LOG_FILE}
-  ls -Rlsth ${datastore_dir}"/" | tee -a ${LOG_FILE}
-  df ${datastore_dir}"/" | tee -a ${LOG_FILE}
-  du -h ${datastore_dir}"/" | tee -a ${LOG_FILE}
+  log_header "Datastore information"
+  execute_simple ls -Rlsth ${datastore_dir}"/"
+  execute_simple df ${datastore_dir}"/"
+  execute_simple du -h ${datastore_dir}"/"
 
   if ${NO_CLEANING_FILES_AT_END}; then
     echo "Do not delete the used directory"
   else
     echo "Delete the used directory"
-    rm -rf ${datastore_dir}
+    execute_simple rm -rf ${datastore_dir}
   fi
-}
-
-main() {
-  run
 }
 
 main "$@"
