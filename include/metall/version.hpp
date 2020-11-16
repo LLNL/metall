472c4a0c82f1c8c19c10f1f1a0e1735436d2239a
Keita Iwabuchi <kiwabuchi@llnl.gov>
2020-11-16 13:56:50 -0800

    Increased internal Metall version number to 0.7.

--- include/metall/version.hpp
+++ include/metall/version.hpp
@@ -16,7 +16,7 @@
 ///  METALL_VERSION / 100 % 1000 // the minor version.
 ///  METALL_VERSION % 100 // the patch level.
 /// \endcode
-#define METALL_VERSION 600
+#define METALL_VERSION 700
 
 namespace metall {
 /// \brief Variable type to handle a version data.
