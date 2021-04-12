3380b717fea656cb08c754a9d3cd7daae23ef73a
Keita Iwabuchi <kiwabuchi@llnl.gov>
2021-04-12 17:36:38 +0900

    Add parallel file copy/clone files.

--- include/metall/detail/file_clone.hpp
+++ include/metall/detail/file_clone.hpp
@@ -82,11 +82,11 @@ inline bool clone_file_macos(const std::string &source_path, const std::string &
 #endif
 }// namespace file_clone_detail
 
-/// \brief Clones a file. If file cloning is not supported, copies the file normally
-/// \param source_path A path to the file to be cloned
-/// \param destination_path A path to copy to
+/// \brief Clones a file. If file cloning is not supported, copies the file normally.
+/// \param source_path A path to the file to be cloned.
+/// \param destination_path A path to copy to.
 /// \return On success, returns true. On error, returns false.
-inline bool clone_file(const std::string &source_path, const std::string &destination_path, const bool sync) {
+inline bool clone_file(const std::string &source_path, const std::string &destination_path) {
   bool ret = false;
 #if defined(__linux__)
   ret = file_clone_detail::clone_file_linux(source_path, destination_path);
@@ -109,11 +109,24 @@ inline bool clone_file(const std::string &source_path, const std::string &destin
     logger::out(logger::level::error, __FILE__, __LINE__, "Failed to copy " + source_path + " to " + destination_path);
 #endif
 
-  if (ret && sync) {
+  if (ret) {
     ret &= metall::mtlldetail::fsync(destination_path);
   }
 
   return ret;
+}
+
+/// \brief Clone files in a directory.
+/// This function does not clone files in subdirectories.
+/// \param source_dir_path A path to source directory.
+/// \param destination_dir_path A path to destination directory.
+/// \param max_num_threads The maximum number of threads to use.
+/// If 0 is given, it is automatically determined.
+/// \return  On success, returns true. On error, returns false.
+inline bool clone_files_in_directory_in_parallel(const std::string &source_dir_path,
+                                                 const std::string &destination_dir_path,
+                                                 const int max_num_threads) {
+  return copy_files_in_directory_in_parallel_helper(source_dir_path, destination_dir_path, max_num_threads, clone_file);
 }
 
 } // namespace metall::mtlldetail
