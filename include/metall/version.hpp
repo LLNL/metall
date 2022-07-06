bb61595f3433c89add23675a5412cd962fc04649
Keita Iwabuchi <iwabuchi1@llnl.gov>
2022-07-06 16:36:18 -0700

    Release v0.21

--- include/metall/version.hpp
+++ include/metall/version.hpp
@@ -14,7 +14,7 @@
 ///  METALL_VERSION / 100 % 1000 // the minor version.
 ///  METALL_VERSION % 100 // the patch level.
 /// \endcode
-#define METALL_VERSION 2000
+#define METALL_VERSION 2100
 
 namespace metall {
 /// \brief Variable type to handle a version data.
