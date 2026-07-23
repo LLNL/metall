ed122ddc315cccd5172b3f89dced402cd93201cc
Keita Iwabuchi <kiwabuchi@llnl.gov>
2026-07-23 04:07:38 +0000

    Refactor run_intensive_test.sh for improved readability and organization

--- scripts/release_test/run_intensive_test.sh
+++ scripts/release_test/run_intensive_test.sh
@@ -24,8 +24,8 @@
 #######################################
 main() {
   readonly METALL_ROOT_DIR=${PWD}
-  source ${METALL_ROOT_DIR}/scripts/test_kernel.sh
-  source ${METALL_ROOT_DIR}/scripts/test_utility.sh
+  source "${METALL_ROOT_DIR}/scripts/test_kernel.sh"
+  source "${METALL_ROOT_DIR}/scripts/test_utility.sh"
 
   echo "Build and test on ${HOSTNAME}"
   show_system_info
@@ -41,57 +41,51 @@ main() {
   build_docs
 
   for BUILD_TYPE in Debug RelWithDebInfo Release; do
-    for DISABLE_FREE_FILE_SPACE in true false; do
-      for USE_SORTED_BIN in true false; do
-        for FREE_SMALL_OBJECT_SIZE_HINT in 0 8 4096 65536; do
-          for METALL_DISABLE_FREE_FILE_SPACE in true false; do
-            for METALL_DISABLE_OBJECT_CACHE in true false; do
-              for USE_ANONYMOUS_NEW_MAP in true false; do
+    for USE_SORTED_BIN in true false; do
+      for FREE_SMALL_OBJECT_SIZE_HINT in 0 8 4096 65536; do
+        for METALL_DISABLE_FREE_FILE_SPACE in true false; do
+          for METALL_DISABLE_OBJECT_CACHE in true false; do
+            for USE_ANONYMOUS_NEW_MAP in true false; do
 
-                DEFS="METALL_VERBOSE_SYSTEM_SUPPORT_WARNING;"
+              DEFS="METALL_VERBOSE_SYSTEM_SUPPORT_WARNING;"
 
-                if [[ "${DISABLE_FREE_FILE_SPACE}" == "true" ]]; then
-                  DEFS="${DEFS}METALL_DISABLE_FREE_FILE_SPACE;"
-                fi
+              if [[ "${USE_SORTED_BIN}" == "true" ]]; then
+                DEFS="${DEFS}METALL_USE_SORTED_BIN;"
+              fi
 
-                if [[ "${USE_SORTED_BIN}" == "true" ]]; then
-                  DEFS="${DEFS}METALL_USE_SORTED_BIN;"
-                fi
+              if [[ "${FREE_SMALL_OBJECT_SIZE_HINT}" -gt 0 ]]; then
+                DEFS="${DEFS}METALL_FREE_SMALL_OBJECT_SIZE_HINT=${FREE_SMALL_OBJECT_SIZE_HINT};"
+              fi
 
-                if [ "${FREE_SMALL_OBJECT_SIZE_HINT}" -gt 0 ]; then
-                  DEFS="${DEFS}METALL_FREE_SMALL_OBJECT_SIZE_HINT=${FREE_SMALL_OBJECT_SIZE_HINT};"
-                fi
+              if [[ "${METALL_DISABLE_FREE_FILE_SPACE}" == "true" ]]; then
+                DEFS="${DEFS}METALL_DISABLE_FREE_FILE_SPACE;"
+              fi
 
-                if [[ "${METALL_DISABLE_FREE_FILE_SPACE}" == "true" ]]; then
-                  DEFS="${DEFS}METALL_DISABLE_FREE_FILE_SPACE;"
-                fi
+              if [[ "${METALL_DISABLE_OBJECT_CACHE}" == "true" ]]; then
+                DEFS="${DEFS}METALL_DISABLE_OBJECT_CACHE;"
+              fi
 
-                if [[ "${METALL_DISABLE_OBJECT_CACHE}" == "true" ]]; then
-                  DEFS="${DEFS}METALL_DISABLE_OBJECT_CACHE;"
-                fi
+              if [[ "${USE_ANONYMOUS_NEW_MAP}" == "true" ]]; then
+                DEFS="${DEFS}METALL_USE_ANONYMOUS_NEW_MAP;"
+              fi
 
-                if [[ "${USE_ANONYMOUS_NEW_MAP}" == "true" ]]; then
-                  DEFS="${DEFS}METALL_USE_ANONYMOUS_NEW_MAP;"
-                fi
+              run_build_and_test_kernel \
+                -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+                -DBUILD_UTILITY=ON \
+                -DBUILD_DOC=OFF \
+                -DBUILD_C=ON \
+                -DBUILD_SHARED_LIBS=ON \
+                -DBUILD_EXAMPLE=ON \
+                -DBUILD_TEST=ON \
+                -DRUN_LARGE_SCALE_TEST=ON \
+                -DRUN_BUILD_AND_TEST_WITH_CI=OFF \
+                -DBUILD_BENCH=ON \
+                -DBUILD_VERIFICATION=OFF \
+                -DCOMPILER_DEFS="${DEFS}" \
+                " ${METALL_CMAKE_ADDITIONAL_OPTIONS} "
 
-                run_build_and_test_kernel \
-                  -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
-                  -DBUILD_UTILITY=ON \
-                  -DBUILD_DOC=OFF \
-                  -DBUILD_C=ON \
-                  -DBUILD_SHARED_LIBS=ON \
-                  -DBUILD_EXAMPLE=ON \
-                  -DBUILD_TEST=ON \
-                  -DRUN_LARGE_SCALE_TEST=ON \
-                  -DRUN_BUILD_AND_TEST_WITH_CI=OFF \
-                  -DBUILD_BENCH=ON \
-                  -DBUILD_VERIFICATION=OFF \
-                  -DCOMPILER_DEFS="${DEFS}" \
-                  " ${METALL_CMAKE_ADDITIONAL_OPTIONS} "
-
-                done
-              done
             done
+          done
         done
       done
     done
